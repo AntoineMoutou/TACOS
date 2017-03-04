dfe8034a1a7b0e4d5f79557b092f9fd3eb05a31c
AntoineMoutou <moutou.antoine@hotmail.fr>
2017-03-04 17:25:53 +0100

    Add files via upload

--- js/geo.js
+++ js/geo.js
@@ -2,16 +2,19 @@ var mymap, lati, long, lastLati, lastLong, lgMarkers, tacosIcon, polyline, polyl
 
 function set_tacos() {
 
-  firstConnection = new Date().getTime();
-  vitesse = 40;
-  lati = 0;
-  long = 0;
-  mymap = L.map('map').setView([lati,long], 3);
-  lgMarkers = new L.LayerGroup();
-  lgPolyline = new L.LayerGroup();
-  lgPolyline2 = new L.LayerGroup();
-  tacosIcon = L.icon({iconUrl : 'img/tacos2.png',iconSize:[76,48]})
+  //Initialisation des diverses variables
 
+  firstConnection = new Date().getTime(); //date de première connexion
+  vitesse = 40; //coefficient multiplicatif pour la vitesse du mode débug
+  lati = 0; //latitude initiale
+  long = 0; //longitude initiale
+  mymap = L.map('map').setView([lati,long], 3); //position de départ de la carte
+  lgMarkers = new L.LayerGroup(); //couche des marqueurs
+  lgPolyline = new L.LayerGroup(); //couche de la polyline du mode standard
+  lgPolyline2 = new L.LayerGroup(); //couche de la polyline du mode débug
+  tacosIcon = L.icon({iconUrl : 'img/tacos2.png',iconSize:[76,48]}); //icone du marqueur special tacos
+
+  //fond de carte OpenStreetMap
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
     {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mymap);
@@ -20,37 +23,49 @@ function set_tacos() {
 
 function set_button() {
 
+  //Fonction d'initialisation de l'écouteur d'événement sur le bouton
+
   document.getElementById("button").addEventListener("click", function(){
 
-  create_photo();
-  create_texte_2();
+    //Fonction permettant de créer la photo selon la position et le zoom choisi et le texte selon la position de l'ISS
+
+    create_photo();
+    create_texte_2();
 
   });
 }
 
 function set_cb() {
 
+  //Fonction d'initialisation de l'écouteur d'événement sur la cbox correspondant au mode débug
+
   document.getElementById("cbox2").addEventListener("click", function(){
 
-  lastLati = 0;
-  lastLong = 365;
+    //Fonction permettant d'éviter que la première polyline après un changement de mode ne sois un trait entre l'ancienne position défini dans un mode (standard/débug) et la nouvelle position définie dans l'autre mode
+
+    lastLati = 0; //latitude réinitialisée
+    lastLong = 365; //longitude réinitialisé à une valeur aberrante afin de ne pas écrire ce tronçon grâce à la condition (lastLong < long)
 
   });
 }
 
 function keep_last_position() {
 
+  //Fonction permettant de récupérer la position dans deux variables afin de pouvoir tracer la prochaine polyline
+
   lati = parseFloat(document.getElementById("latitude").innerText);
   long = parseFloat(document.getElementById("longitude").innerText);
 
   if (!(lati=="")) {
-    lastLati = lati;
-    lastLong = long;
+    lastLati = lati; //maj de la latitude
+    lastLong = long; //maj de la longitude
   }
 
 }
 
 function update_position(){
+
+  //Fonction pour récupérer la position avec la méthode décrite dans la partie 1
 
   // création de l'objet xhr
   var ajax = new XMLHttpRequest();
@@ -84,7 +99,9 @@ function update_position(){
 
 function update_position_2(){
 
-  vitesse = document.getElementById("vitesse").value;
+  //Fonction pour récupérer la position avec la méthode décrite dans la partie 3 (API maison)
+
+  vitesse = document.getElementById("vitesse").value; //Récupération de la valeur du slider vitesse
 
   // création de l'objet xhr
   var ajax = new XMLHttpRequest();
@@ -105,8 +122,6 @@ function update_position_2(){
     // si l'état est le numéro 4 et que la ressource est trouvée
     if(ajax.readyState == 4 && ajax.status == 200) {
       var jsonString = ajax.responseText;
-
-
       var jsonObj = JSON.parse(jsonString);
       var latitude = jsonObj.latitude;
       var longitude = jsonObj.longitude;
@@ -114,7 +129,6 @@ function update_position_2(){
       document.getElementById("latitude").innerHTML = latitude;
       document.getElementById("longitude").innerHTML = longitude;
 
-      console.log(jsonString);
     }
 
   });
@@ -126,24 +140,30 @@ function update_position_2(){
 
 function update_map() {
 
+  //Fonction permettant de suivre l'ISS
+
   lati = parseFloat(document.getElementById("latitude").innerText);
   long = parseFloat(document.getElementById("longitude").innerText);
 
-  mymap.setView([lati, long], 6);
+  mymap.setView([lati, long], 6); //recentrage de la carte
 
 }
 
 function update_marker() {
 
+  //Fonction permettant d'actualiser la position du marqueur
+
   lati = parseFloat(document.getElementById("latitude").innerText);
   long = parseFloat(document.getElementById("longitude").innerText);
 
-  L.marker([lati, long], {icon: tacosIcon}).addTo(lgMarkers);
-  mymap.addLayer(lgMarkers);
+  L.marker([lati, long], {icon: tacosIcon}).addTo(lgMarkers); //ajout du marqueur à la couche des marqueurs
+  mymap.addLayer(lgMarkers); //ajout de la couche des marqueurs à la carte
 
 }
 
 function layer_remove_markers() {
+
+  //Fonction permettant de nettoyer la couche des marqueurs
 
   lgMarkers.clearLayers();
 
@@ -151,11 +171,15 @@ function layer_remove_markers() {
 
 function layer_remove_polyline() {
 
+  //Fonction permettant de nettoyer la couche de la polyline du mode standard
+
   lgPolyline.clearLayers();
 
 }
 
 function layer_remove_polyline_2() {
+
+  //Fonction permettant de nettoyer la couche de la polyline du mode débug
 
   lgPolyline2.clearLayers();
 
@@ -163,40 +187,46 @@ function layer_remove_polyline_2() {
 
 function update_polyline() {
 
+  //Fonction permettant de créer la ployline du mode standard
+
   lati = parseFloat(document.getElementById("latitude").innerText);
   long = parseFloat(document.getElementById("longitude").innerText);
 
   if (lastLong < long) {
     var coord = [new L.LatLng(lastLati, lastLong),new L.LatLng(lati,long)];
-    var polyline = new L.polyline(coord).addTo(lgPolyline);
-    mymap.addLayer(lgPolyline);
+    var polyline = new L.polyline(coord).addTo(lgPolyline); //ajout de la polyline à la couche de polyline
+    mymap.addLayer(lgPolyline); //ajout de la couche de polyline à la carte
   }
 
 }
 
 function update_polyline_2() {
 
+  //Fonction permettant de créer la ployline du mode débug
+
   lati = parseFloat(document.getElementById("latitude").innerText);
   long = parseFloat(document.getElementById("longitude").innerText);
 
   if (lastLong < long) {
     var coord = [new L.LatLng(lastLati, lastLong),new L.LatLng(lati,long)];
-    var polyline2 = new L.polyline(coord).addTo(lgPolyline2);
-    mymap.addLayer(lgPolyline2);
-    console.log("cool");
+    var polyline2 = new L.polyline(coord).addTo(lgPolyline2); //ajout de la polyline à la couche de polyline
+    mymap.addLayer(lgPolyline2); //ajout de la couche de polyline à la carte
   }
 
 }
 
 function create_photo() {
 
+  //Fonction permettant de créer la photo
+
   lati = parseFloat(document.getElementById("latitude").innerText);
   long = parseFloat(document.getElementById("longitude").innerText);
 
   var zoomlist = document.getElementsByName("zoom");
   var zoom = 3;
-  var bearing = Math.floor(Math.random()*360);
+  var bearing = Math.floor(Math.random()*360); //orientation aléatoire
 
+  //récupération de la valeur du zoom
   for (var i = 0; i < zoomlist.length; i++) {
     if (zoomlist[i].checked) {
       zoom = zoomlist[i].value;
@@ -271,7 +301,7 @@ function create_texte_2() {
   ajax2.open('GET', url, true);
 
   // métadonnées de la requête AJAX
-  // ajax2.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  ajax2.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
   // evenement de changement d'état de la requête
   ajax2.addEventListener('readystatechange',  function(e) {
@@ -301,6 +331,8 @@ function create_texte_2() {
 
 function main() {
 
+  //Fonction principale
+
   if (document.getElementById("cbox2").checked) {
     update_position_2();
     layer_remove_polyline();
@@ -321,7 +353,6 @@ function main() {
 
   update_marker();
   keep_last_position();
-
 
 }
 
